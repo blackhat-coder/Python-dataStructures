6ef9408ad710c140e47d5e1c5d58bf5a3155e486
blackhat-coder <57095771+blackhat-coder@users.noreply.github.com>
2020-02-25 14:06:16 +0100

    Create longest_word_problem.py

---
+++ longest_word_problem.py
@@ -0,0 +1,16 @@
+
+Given a sentence from the user, print the longest word in the sentence
+
+sentence = input("sentence: ")
+
+words = sentence.split()
+longest_word = ''
+
+for i in range(len(words)):
+    try:
+        if len(words[i]) > len(words[i+1]):
+            largest = words[i]
+    except Exception:
+        largest = words[i]
+
+print(longest_word)
